e4b29a4fe28888bf6f6b4fc01d61757f6a6c5865
squidfunk <martin.donath@squidfunk.com>
2022-06-08 20:21:22 +0200

    Revert last commit

--- src/assets/javascripts/integrations/search/_/index.ts
+++ src/assets/javascripts/integrations/search/_/index.ts
@@ -208,7 +208,6 @@ export class Search {
           tables[document.location][field] = []
           if (Array.isArray(document[field])) {
             const values = document[field] as string[]
-            // lunr.tokenizer(
             return values.map(value => ({
               toString() { return value },
               table: tables[document.location][field]
