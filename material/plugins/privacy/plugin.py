c86980461cefa779d2c20462b9ff3de905bd4b44
squidfunk <martin.donath@squidfunk.com>
2022-10-16 18:46:49 +0700

    Refactored privacy plugin and added support for auto rel=noopener on target=_blank

--- material/plugins/privacy/plugin.py
+++ material/plugins/privacy/plugin.py
@@ -31,6 +31,7 @@ from mkdocs.commands.build import DuplicateFilter
 from mkdocs.config import config_options as opt
 from mkdocs.config.base import Config
 from mkdocs.plugins import BasePlugin
+from mkdocs.structure.files import File
 from urllib.parse import urlparse
 
 # -----------------------------------------------------------------------------
@@ -45,15 +46,21 @@ class PrivacyPluginConfig(Config):
     cache = opt.Type(bool, default = True)
     cache_dir = opt.Type(str, default = ".cache/plugin/privacy")
 
-    # Options for external assets
-    externals = opt.Choice(("bundle", "report"), default = "bundle")
-    externals_dir = opt.Type(str, default = "assets/externals")
-    externals_exclude = opt.Type(list, default = [])
+    # Options for external assets and links
+    external_assets = opt.Choice(("bundle", "report"), default = "bundle")
+    external_assets_dir = opt.Type(str, default = "assets/externals")
+    external_assets_exclude = opt.Type(list, default = [])
+    external_links = opt.Type(bool, default = True),
+    external_links_attr_map = opt.Type(dict, default = dict())
+    external_links_noopener = opt.Type(bool, default = True)
 
     # Deprecated options
     download = opt.Deprecated(moved_to = "enabled")
-    download_directory = opt.Deprecated(moved_to = "externals_dir")
-    externals_directory = opt.Deprecated(moved_to = "externals_dir")
+    download_directory = opt.Deprecated(moved_to = "external_assets_dir")
+    externals = opt.Deprecated(moved_to = "external_assets")
+    externals_dir = opt.Deprecated(moved_to = "external_assets_dir")
+    externals_directory = opt.Deprecated(moved_to = "external_assets_dir")
+    externals_exclude = opt.Deprecated(moved_to = "external_assets_exclude")
 
 # -----------------------------------------------------------------------------
 
@@ -62,38 +69,37 @@ class PrivacyPlugin(BasePlugin[PrivacyPluginConfig]):
 
     # Initialize plugin
     def on_config(self, config):
-        self.site_url = urlparse(config.site_url)
-        self.site_dir = config.site_dir
-        self.files = []
+        self.site  = urlparse(config.site_url)
+        self.queue = []
 
-    # Determine files that need to be post-processed
+    # Determine files that might include external assets
     def on_files(self, files, *, config):
         if not self.config.enabled:
             return
 
-        # Filter relevant files, short-circuit Lunr.js
+        # Enqueue files for processing, but short-circuit Lunr.js
         for file in files:
             if file.url.endswith(".js") or file.url.endswith(".css"):
                 if not "assets/javascripts/lunr" in file.url:
-                    self.files.append(file)
+                    self.queue.append(file)
 
         # If site URL is not given, add Mermaid.js - see https://bit.ly/36tZXsA
         # This is a special case, as Material for MkDocs automatically loads
         # Mermaid.js when a Mermaid diagram is found in the page.
         if not config.site_url:
-            if not any("mermaid" in js for js in config.extra_javascript):
+            if not any("mermaid" in url for url in config.extra_javascript):
                 config.extra_javascript.append(
-                    "https://unpkg.com/mermaid@9.0.1/dist/mermaid.min.js"
+                    "https://unpkg.com/mermaid@9.1.7/dist/mermaid.min.js"
                 )
 
-    # Parse, fetch and store external assets in pages
+    # Parse, fetch and store external assets and patch links
     def on_post_page(self, output, *, page, config):
         if not self.config.enabled:
             return
 
-        # Find all external resources
+        # Find all external assets and links
         expr = re.compile(
-            r"<(?:link[^>]+href|(?:script|img)[^>]+src)=['\"]?http[^>]+>",
+            r"<(?:(?:a|link)[^>]+href|(?:script|img)[^>]+src)=['\"]?http[^>]+>",
             re.IGNORECASE | re.MULTILINE
         )
 
@@ -103,109 +109,124 @@ class PrivacyPlugin(BasePlugin[PrivacyPluginConfig]):
 
             # Compute offsets for replacement
             l = match.start()
-            r = l + len(value)
+            r = match.end()
 
-            # Handle preconnect hints and style sheets
+            # Handle external links 
             el = html.fragment_fromstring(value)
+            if self.config.external_links and el.tag == "a":
+                for key, value in self.config.external_links_attr_map.items():
+                    el.set(key, value)
+
+                # Set rel="noopener" if link opens in a new window
+                if self.config.external_links_noopener:
+                    if el.get("target") == "_blank":
+                        el.set("rel", "noopener")
+
+                # Replace link opening tag (without closing tag)
+                output = "".join([
+                    output[:l],
+                    html.tostring(el, encoding = "unicode")[:-4],
+                    output[r:]
+                ])
+
+            # Handle external style sheet or preconnect hint
             if el.tag == "link":
                 raw = el.get("href", "")
 
-                # Check if URL is external
+                # Only process external assets
                 url = urlparse(raw)
                 if not self._is_external(url):
                     continue
 
-                # Replace external preconnect hint in output
+                # Replace external preconnect hint
                 rel = el.get("rel")
                 if rel == "preconnect":
-                    output = output[0:l] + output[r:]
+                    output = output[:l] + output[r:]
 
-                # Replace external style sheet in output
+                # Replace external style sheet
                 if rel == "stylesheet":
                     output = "".join([
                         output[:l],
-                        value.replace(raw, self._fetch(url, page)),
+                        value.replace(raw, self._fetch(url, page.file, config)),
                         output[r:]
                     ])
 
-            # Handle external scripts and images
+            # Handle external script or image
             if el.tag == "script" or el.tag == "img":
                 raw = el.get("src", "")
 
-                # Check if URL is external
+                # Only process external assets
                 url = urlparse(raw)
                 if not self._is_external(url):
                     continue
 
-                # Replace external script or image in output
+                # Replace external script or image
                 output = "".join([
                     output[:l],
-                    value.replace(raw, self._fetch(url, page)),
+                    value.replace(raw, self._fetch(url, page.file, config)),
                     output[r:]
                 ])
 
         # Return output with replaced occurrences
         return output
 
-    # Parse, fetch and store external assets in assets
+    # Parse, fetch and store external assets in style sheets and scripts
     def on_post_build(self, *, config):
         if not self.config.enabled:
             return
 
-        # Check all files that are part of the build
-        for file in self.files:
-            path = file.abs_dest_path
-            if not os.path.isfile(path):
+        # Process queued files
+        for file in self.queue:
+            if not os.path.isfile(file.abs_dest_path):
                 continue
 
-            # Handle internal style sheet or script
-            if path.endswith(".css") or path.endswith(".js"):
-                with open(path, encoding = "utf-8") as f:
-                    utils.write_file(
-                        self._fetch_dependents(f.read(), file.dest_path),
-                        path
-                    )
+            # Open file and patch dependent assets
+            with open(file.abs_dest_path, encoding = "utf-8") as f:
+                utils.write_file(
+                    self._fetch_dependents(f.read(), file, config),
+                    file.abs_dest_path
+                )
 
     # -------------------------------------------------------------------------
 
     # Check if the given URL is external
     def _is_external(self, url):
-        return url.hostname != self.site_url.hostname
+        return url.hostname != self.site.hostname
 
     # Check if the given URL is excluded
     def _is_excluded(self, url, base):
         url = re.sub(r"^https?:\/\/", "", url)
-        for pattern in self.config.externals_exclude:
+        for pattern in self.config.external_assets_exclude:
             if fnmatch(url, pattern):
                 log.debug(f"Excluding external file in '{base}': {url}")
                 return True
 
         # Report external assets if bundling is not enabled
-        if self.config.externals == "report":
+        if self.config.external_assets == "report":
             log.warning(f"External file in '{base}': {url}")
             return True
 
-    # Fetch external resource in given page
-    def _fetch(self, url, page):
+    # Fetch external asset in the context of a page
+    def _fetch(self, url, base, config):
         raw = url.geturl()
 
-        # Check if URL is excluded
-        if self._is_excluded(raw, page.file.dest_uri):
+        # Skip excluded files
+        if self._is_excluded(raw, base.dest_uri):
             return raw
 
-        # Compute destination file system path
-        file = os.path.join(self.config.cache_dir, self._resolve(url))
-        path = file = os.path.normpath(file)
+        # Resolve file and check if it needs to be downloaded
+        file = self._map_url_to_file(url, config)
+        if not os.path.isfile(file.abs_src_path) or not self.config.cache:
+            path = file.abs_src_path
 
-        # Download file if it's not contained in the cache
-        if not os.path.isfile(file) or not self.config.cache:
+            # Download external file
             log.debug(f"Downloading external file: {raw}")
             res = requests.get(raw, headers = {
 
                 # Set user agent explicitly, so Google Fonts gives us *.woff2
                 # files, which according to caniuse.com is the only format we
                 # need to download as it covers the entire range of browsers
-                # we're officially supporting
+                # we're officially supporting.
                 "User-Agent": " ".join([
                     "Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
                     "AppleWebKit/537.36 (KHTML, like Gecko)",
@@ -214,65 +235,66 @@ class PrivacyPlugin(BasePlugin[PrivacyPluginConfig]):
             })
 
             # Compute and ensure presence of file extension
-            name = re.findall(r"^[^;]+", res.headers["content-type"])[0]
-            extension = extensions.get(name)
-            if extension and not file.endswith(extension):
-                file, _ = posixpath.splitext(file)
-                file += extension
+            mime = re.findall(r"^[^;]+", res.headers["content-type"])[0]
+            extension = extensions.get(mime)
+            if extension and not path.endswith(extension):
+                path, _ = os.path.splitext(path)
+                path += extension
 
-            # Write contents and create symbolic link if necessary
-            utils.write_file(res.content, file)
-            if path != file:
+            # Write contents and create symlink if no extension was present
+            utils.write_file(res.content, file.abs_src_path)
+            if path != file.abs_src_path:
 
-                # Creating symbolic links might fail on Windows. Thus, we just
-                # print a warning and continue - see https://bit.ly/3xYFzcZ
+                # Creating symlinks might fail on Windows. Thus, we just print
+                # a warning and continue - see https://bit.ly/3xYFzcZ
                 try:
-                    os.symlink(os.path.basename(file), path)
+                    os.symlink(os.path.basename(file.abs_src_path), path)
                 except OSError:
-                    log.warning(f"Couldn't create symbolic link '{file}'")
-
-        # Append file extension from file after resolving symbolic links
-        _, extension = os.path.splitext(os.path.realpath(file))
-        if not file.endswith(extension):
-            file, _ = os.path.splitext(file)
-            file += extension
-
-        # Append file extension to destination URL
-        path = posixpath.join(self.config.externals_dir, self._resolve(url))
-        if not path.endswith(extension):
-            path += extension
-
-        # Compute final path relative to output directory
-        full = os.path.normpath(os.path.join(self.site_dir, path))
-        if not os.path.exists(full):
-
-            # Open file and patch dependent resources
-            if extension == ".css" or extension == ".js":
-                with open(file, encoding = "utf-8") as f:
-                    utils.write_file(
-                        self._fetch_dependents(f.read(), path),
-                        full
+                    log.warning(
+                        f"Couldn't create symbolic link '{file.src_uri}'"
                     )
 
-            # Copy file from cache to output directory
+        # Resolve destination if file points to a symlink
+        _, extension = os.path.splitext(file.abs_src_path)
+        if os.path.islink(file.abs_src_path):
+            file.abs_src_path = os.path.realpath(file.abs_src_path)
+            _, extension = os.path.splitext(file.abs_src_path)
+
+            # Compute destination file system path
+            file.dest_uri += extension
+            file.abs_dest_path += extension
+
+            # Compute destination URL
+            file.url += extension
+
+        # Copy or open and patch file
+        if not os.path.isfile(file.abs_dest_path):
+            if not extension == ".css" and not extension == ".js":
+                file.copy_file()
+
+            # Open file and patch dependent assets
             else:
-                utils.copy_file(file, full)
+                with open(file.abs_src_path, encoding = "utf-8") as f:
+                    utils.write_file(
+                        self._fetch_dependents(f.read(), file, config),
+                        file.abs_dest_path
+                    )
 
         # Return URL relative to current page
-        return utils.get_relative_url(path, page.url)
+        return file.url_relative_to(base)
 
-    # Fetch dependent resources in external assets
-    def _fetch_dependents(self, output, base):
+    # Fetch dependent assets in external assets
+    def _fetch_dependents(self, output, base, config):
 
         # Fetch external assets in style sheet
-        if base.endswith(".css"):
+        if base.url.endswith(".css"):
             expr = re.compile(
                 r"url\((\s*http?[^)]+)\)",
                 re.IGNORECASE | re.MULTILINE
             )
 
         # Fetch external assets in script
-        elif base.endswith(".js"):
+        elif base.url.endswith(".js"):
             expr = re.compile(
                 r"[\"'](http[^\"']+\.js)[\"']",
                 re.IGNORECASE | re.MULTILINE
@@ -285,37 +307,35 @@ class PrivacyPlugin(BasePlugin[PrivacyPluginConfig]):
 
             # Compute offsets for replacement
             l = match.start()
-            r = l + len(value)
+            r = match.end()
 
-            # Check if URL is external
+            # Only process external files
             url = urlparse(raw)
             if not self._is_external(url):
                 continue
 
-            # Check if URL is excluded
-            if self._is_excluded(raw, base):
+            # Skip excluded files
+            if self._is_excluded(raw, base.dest_uri):
                 continue
 
-            # Download file if it's not contained in the cache
-            file = os.path.join(self.config.cache_dir, self._resolve(url))
-            if not os.path.isfile(file) or not self.config.cache:
+            # Resolve file and check if it needs to be downloaded
+            file = self._map_url_to_file(url, config)
+            if not os.path.isfile(file.abs_src_path) or not self.config.cache:
+
+                # Download external file
                 log.debug(f"Downloading external file: {raw}")
                 res = requests.get(raw)
-                utils.write_file(res.content, file)
 
-            # Compute final path relative to output directory
-            path = posixpath.join(
-                self.config.externals_dir,
-                self._resolve(url)
-            )
-
-            # Create relative URL for asset in style sheet
-            if base.endswith(".css"):
-                url = utils.get_relative_url(path, base)
+                # Write contents
+                utils.write_file(res.content, file.abs_src_path)
 
             # Create absolute URL for asset in script
-            elif base.endswith(".js"):
-                url = posixpath.join(self.site_url.geturl(), path)
+            if base.url.endswith(".js"):
+                url = posixpath.join(config.site_url, file.url)
+
+            # Create relative URL for everything else
+            else:
+                url = file.url_relative_to(base)
 
             # Replace external asset in output
             output = "".join([
@@ -324,17 +344,22 @@ class PrivacyPlugin(BasePlugin[PrivacyPluginConfig]):
                 output[r:]
             ])
 
-            # Copy file from cache to output directory
-            full = os.path.join(self.site_dir, path)
-            utils.copy_file(file, full)
+            # Copy file to target directory
+            file.copy_file()
 
         # Return output with replaced occurrences
         return bytes(output, encoding = "utf8")
 
-    # Resolve file name from URL
-    def _resolve(self, url):
+    # Resolve a file for a URL
+    def _map_url_to_file(self, url, config):
         data = url._replace(scheme = "", query = "", fragment = "")
-        return data.geturl()[2:]
+        base = self.config.external_assets_dir
+        return File(
+            posixpath.join(base, data.geturl()[2:]),
+            self.config.cache_dir, 
+            config.site_dir,
+            False
+        )
 
 # -----------------------------------------------------------------------------
 # Data
