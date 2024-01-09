8721121c2ce4151d86fe868d5f8c9ca7257b9708
squidfunk <martin.donath@squidfunk.com>
2024-01-09 08:50:54 +0700

    Updated copyright year

--- material/utilities/filter/__init__.py
+++ material/utilities/filter/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016-2023 Martin Donath <martin.donath@squidfunk.com>
+# Copyright (c) 2016-2024 Martin Donath <martin.donath@squidfunk.com>
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to
@@ -17,6 +17,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
+
+from __future__ import annotations
 
 import logging
 
